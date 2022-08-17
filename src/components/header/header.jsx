a2f00848b4264ac38997904c8a8806923eb6c00c
hheeseung <ssigu69@gmail.com>
2022-08-17 17:54:23 +0900

    CSS: edit header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,11 +4,9 @@ import styles from './header.module.css';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.header__title}>Movie Web Service</h1>
-      <p className={styles.header__description}>
-        This site allows you to view ratings and information about the top 50
-        movies on the YTS site.
-      </p>
+      <h1 className={styles.header__title}>
+        <i className='fa-solid fa-clapperboard' /> Movie Web Service
+      </h1>
     </header>
   );
 };
