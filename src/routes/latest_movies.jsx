618112e4030cdaff8117490cc09038b31c02bc43
hheeseung <ssigu69@gmail.com>
2022-09-26 13:12:47 +0900

    add loading

--- src/routes/latest_movies.jsx
+++ src/routes/latest_movies.jsx
@@ -1,30 +1,41 @@
 import React, {useEffect, useState} from 'react';
+import Loader from '../components/loader/loader';
 import MovieList from '../components/movie_list/movie_list';
 import Navigation from '../components/navbar/navigation';
 import styles from './movie_lists.module.css';
 
 const LatestMovies = ({movieAPI}) => {
   const [recentMovies, setRecentMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     movieAPI
       .movieListAll() //
-      .then((movies) => setRecentMovies(movies[2]));
+      .then((movies) => {
+        setRecentMovies(movies[2]);
+        setLoading(false);
+      });
   }, [movieAPI]);
   return (
     <>
-      <Navigation />
-      <ul className={styles.list}>
-        {recentMovies.map((movie) => (
-          <MovieList
-            key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            cover={movie.medium_cover_image}
-            year={movie.year}
-          />
-        ))}
-      </ul>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navigation />
+          <ul className={styles.list}>
+            {recentMovies.map((movie) => (
+              <MovieList
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                cover={movie.medium_cover_image}
+                year={movie.year}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
--- src/routes/popular_movies.jsx
+++ src/routes/popular_movies.jsx
@@ -1,29 +1,41 @@
 import React, {useEffect, useState} from 'react';
+import Loader from '../components/loader/loader';
 import MovieList from '../components/movie_list/movie_list';
 import Navigation from '../components/navbar/navigation';
 import styles from './movie_lists.module.css';
 
 const PopularMovies = ({movieAPI}) => {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     movieAPI
       .movieListAll() //
-      .then((movies) => setPopularMovies(movies[0]));
+      .then((movies) => {
+        setPopularMovies(movies[0]);
+        setLoading(false);
+      });
   }, [movieAPI]);
   return (
     <>
-      <Navigation />
-      <ul className={styles.list}>
-        {popularMovies.map((movie) => (
-          <MovieList
-            key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            cover={movie.medium_cover_image}
-            year={movie.year}
-          />
-        ))}
-      </ul>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navigation />
+          <ul className={styles.list}>
+            {popularMovies.map((movie) => (
+              <MovieList
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                cover={movie.medium_cover_image}
+                year={movie.year}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
