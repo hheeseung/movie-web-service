204e8c049520886fb475bb2b83ab9e6ffc5562bf
hheeseung <ssigu69@gmail.com>
2022-09-26 19:50:51 +0900

    CSS: remove ul padding, edit navbar font size

--- src/components/navbar/navigation.jsx
+++ src/components/navbar/navigation.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import {useNavigate} from 'react-router-dom';
+import styles from './navigation.module.css';
 
 const Navigation = () => {
   const navigate = useNavigate();
   return (
-    <Navbar bg='dark' variant='dark'>
-      <Container>
-        <Navbar.Brand style={{cursor: 'pointer'}} onClick={() => navigate('/')}>
-          Movie Viewer
-        </Navbar.Brand>
-        <Nav className='me-auto'>
-          <Nav.Link onClick={() => navigate('/movie/popular_movies')}>
-            Popular
-          </Nav.Link>
-          <Nav.Link onClick={() => navigate('/movie/high_rated_movies')}>
-            High Rated
-          </Nav.Link>
-          <Nav.Link onClick={() => navigate('/movie/latest_movies')}>
-            Latest
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <nav className={styles.nav}>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand
+            style={{cursor: 'pointer'}}
+            onClick={() => navigate('/')}
+          >
+            Movie Viewer
+          </Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link onClick={() => navigate('/movie/popular_movies')}>
+              Popular
+            </Nav.Link>
+            <Nav.Link onClick={() => navigate('/movie/high_rated_movies')}>
+              High Rated
+            </Nav.Link>
+            <Nav.Link onClick={() => navigate('/movie/latest_movies')}>
+              Latest
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </nav>
   );
 };
 
