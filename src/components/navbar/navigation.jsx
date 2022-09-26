b7395e83a78366e34dbe257a940f6726e9292597
hheeseung <ssigu69@gmail.com>
2022-09-26 12:52:34 +0900

    Navbar: routing path

--- src/components/navbar/navigation.jsx
+++ src/components/navbar/navigation.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
 
-const Navigation = () => (
-  <Navbar bg='dark' variant='dark'>
-    <Container>
-      <Navbar.Brand href='#home'>Movie Viewer</Navbar.Brand>
-      <Nav className='me-auto'>
-        <Nav.Link href='#home'>Popular</Nav.Link>
-        <Nav.Link href='#features'>High Rated</Nav.Link>
-        <Nav.Link href='#pricing'>Latest</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
-);
+const Navigation = () => {
+  const navigate = useNavigate();
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>Movie Viewer</Navbar.Brand>
+        <Nav className='me-auto'>
+          <Nav.Link onClick={() => navigate('/movie/popular_movies')}>
+            Popular
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate('/movie/high_rated_movies')}>
+            High Rated
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate('/movie/latest_movioes')}>
+            Latest
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Navigation;
